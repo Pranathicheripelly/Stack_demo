9c7c3147cbce89fccc683a550f02169ca763d8ef
Pranathicheripelly <Cheripellypranathi123@gmail.com>
2023-10-31 13:27:38 +0530

    Add project files.

---
+++ Stack_demo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack_demo
+{
+    
+
+       
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                runApp();
+            }
+
+            static void runApp()
+            {
+                stack stack = new stack();
+
+                // Push elements onto the stack
+                stack.Push(20);
+                stack.Push(30);
+                stack.Push(40);
+
+                // Pop elements from the stack and display them
+                Console.WriteLine("Stack Elements:");
+                int element;
+                while (stack.Pop(out element))
+                {
+                    Console.WriteLine(element);
+
+                }
+                Console.ReadLine();
+            }
+        }
+
+    }
+
---
+++ Stack_demo/stack.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack_demo
+{
+   
+        public class Node
+        {
+            public int Data { get; set; }
+            public Node Next { get; set; }
+
+            public Node(int data)
+            {
+                Data = data;
+                Next = null;
+            }
+        }
+
+        public class stack
+        {
+            public Node Top { get; private set; }
+
+            public bool IsEmpty()
+            {
+                return Top == null;
+            }
+
+            public void Push(int data)
+            {
+                Node newNode = new Node(data);
+                newNode.Next = Top;
+                Top = newNode;
+            }
+
+            public bool Pop(out int element)
+            {
+                if (IsEmpty())
+                {
+                    element = 0; // You can choose any default value
+                    return false; // Indicate that the operation failed
+                }
+
+                element = Top.Data;
+                Top = Top.Next;
+                return true; // Indicate a successful pop
+            }
+        }
+
+    }
+
